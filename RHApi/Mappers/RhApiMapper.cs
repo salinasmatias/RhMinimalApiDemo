946d279e025f88f3d71dd65e9cb43bc0acd81446
Konta <salinasmatias1990@gmail.com>
2023-09-25 02:55:42 -0300

    Added Mapper.

---
+++ RHApi/Mappers/RhApiMapper.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using RHApi.Dtos;
+using RHApi.Models;
+
+namespace RHApi.Mappers
+{
+    public class RhApiMapper : Profile
+    {
+        public RhApiMapper()
+        {
+            CreateMap<Country, CountryDto>().ReverseMap();
+            CreateMap<Department, DepartmentDto>().ReverseMap();
+            CreateMap<Employee, EmployeeDto>().ReverseMap();
+            CreateMap<Job, JobDto>().ReverseMap();
+            CreateMap<Location, LocationDto>().ReverseMap();
+            CreateMap<RegionDto, RegionDto>().ReverseMap();
+        }
+    }
+}
